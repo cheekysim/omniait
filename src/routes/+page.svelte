94ef019bf496cfd3950465945116156052faad3b
cheekysim <minecraftcheekysim@gmail.com>
2025-02-15 20:51:26 +0000

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<section class="flex h-dvh w-full flex-col items-center justify-center">
+	<h1 class="text-6xl font-bold">Omnia IT</h1>
+	<p class="text-center text-lg">
+		Welcome to Omnia IT, our website is currently under construction. We appreciate your patience.
+	</p>
+	<p class="text-md text-center text-gray-500">Stay tuned for updates!</p>
+</section>
