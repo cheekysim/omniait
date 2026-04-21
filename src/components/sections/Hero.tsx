940a6c17283c261044ed2605552d73d6c2329dc9
cheekysim <minecraftcheekysim@gmail.com>
2026-04-21 16:35:26 +0100

    Added email to header

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -24,9 +24,15 @@ export default function Hero() {
 
   return (
     <section className="flex min-h-screen flex-col items-center justify-between">
-      <div className="z-10 w-full max-w-5xl items-center justify-around text-sm flex h-full grow">
+      <div className="z-10 w-full max-w-5xl items-center justify-around text-sm flex h-full grow flex-col">
         <p className="text-center lg:text-9xl md:text-8xl text-6xl tracking-wide font-semibold">
           Omnia IT
+          <a
+            className="text-sm block text-primary"
+            href="mailto:euan@omniait.co.uk"
+          >
+            Euan@omniait.co.uk
+          </a>
         </p>
       </div>
       {isMobile ? <HeroSVGMobile /> : <HeroSVG />}
